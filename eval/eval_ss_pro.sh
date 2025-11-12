809f168e8ae959ff8ad2f750cacd7573e3c811ce
sjz5202 <smz5479@outlook.com>
2025-11-11 19:23:26 -0800

    eval

--- eval/eval_ss_pro.sh
+++ eval/eval_ss_pro.sh
@@ -1,10 +1,9 @@
 # smz8599/GUI-AIMA-3B smz8599/GUI-AIMA-3B-kl
-ckpt_name="smz8599/GUI-AIMA-3B-kl"
+ckpt_name="smz8599/GUI-AIMA-3B"
 out_ss_pro="/mnt/localssd/gui_aima_res/ss_pro/GUI-AIMA-3B-kl/"
-# visualization_dir =
 CUDA_VISIBLE_DEVICES=0 python eval/screenSpot_pro.py --model_name_or_path "$ckpt_name" --save_path "$out_ss_pro" # --visualization_dir $visualization_dir
 
+ckpt_name="smz8599/GUI-AIMA-3B-kl"
 #two stage zoom in
 out_ss_pro_two_stage="/mnt/localssd/gui_aima_res/ss_pro/GUI-AIMA-3B-kl/two_stage"
-# visualization_dir =
 CUDA_VISIBLE_DEVICES=1 python eval/screenSpot_pro_two_stage.py --model_name_or_path "$ckpt_name" --save_path "$out_ss_pro_two_stage" # --visualization_dir $visualization_dir
